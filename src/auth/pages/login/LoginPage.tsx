ed82bc7993229a466dfca1180987e7fb30ea4a41
franklen-1 <76359971@continental.edu.pe>
2025-12-01 18:05:18 -0500

    v12

--- src/auth/pages/login/LoginPage.tsx
+++ src/auth/pages/login/LoginPage.tsx
@@ -126,7 +126,7 @@ export const LoginPage = () => {
           </form>
           <div className="relative hidden bg-muted md:block">
             <img
-              src="/placeholder.svg"
+              src="/placeholder.png"
               alt="Image"
               className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
             />
