9c6e59afddde1ad9b3a46dca9ac361e68b9396c4
franklen-1 <76359971@continental.edu.pe>
2025-12-01 17:51:31 -0500

    v11

--- src/components/custom/CustomLogo.tsx
+++ src/components/custom/CustomLogo.tsx
@@ -1,18 +1,22 @@
 import { Link } from 'react-router';
 
-interface Props {
-  subtitle?: string;
-}
+// interface Props {
+//   // subtitle?: string;
+// }
 
-export const CustomLogo = ({ subtitle = 'Shop' }: Props) => {
+export const CustomLogo = () => {
   return (
     <Link to="/" className="flex items-center whitespace-nowrap">
       <span className="font-montserrat font-bold text-xl m-0 whitespace-nowrap">
-        Teslo |
+              <img
+              src="/logo.png"
+              alt="Image"
+              className="h-12 w-15"
+            />
       </span>
-      <p className="text-muted-foreground m-0 px-2 whitespace-nowrap">
-        {subtitle}
-      </p>
+      {/* <p className="text-muted-foreground m-0 px-2 whitespace-nowrap">
+       
+      </p> */}
     </Link>
   );
 };
